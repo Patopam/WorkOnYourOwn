8bf856ad7b486e7f81e1bb7e4da1d3e15dbc9cca
klimga <136005848+klimga@users.noreply.github.com>
2024-02-08 10:42:55 -0500

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,30 @@
+function obtenerProductosCaros(productos) {
+  
+    let productosCaros = [];
+
+    
+    for (let i = 0; i < productos.length; i++) {
+        
+        let productoActual = productos[i];
+
+        
+        if (productoActual.precio > 50) {
+         
+            productosCaros.push(productoActual);
+        }
+    }
+
+  
+    return productosCaros;
+}
+
+
+let productos = [
+    { nombre: "Camisa", precio: 25 },
+    { nombre: "Pantalón", precio: 60 },
+    { nombre: "Zapatos", precio: 40 },
+    { nombre: "Reloj", precio: 80 }
+];
+
+let productosCaros = obtenerProductosCaros(productos);
+console.log(productosCaros); 
